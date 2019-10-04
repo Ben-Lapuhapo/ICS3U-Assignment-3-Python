01980dd3e2249e8888c58e7a44a2a91110a1986e
Ben Lapuhapo <ben.lapuhapo@mths.ca>
2019-10-04 17:21:36 +0000

    Added shopping calculator program

---
+++ constants.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Created by: Ben Lapuhapo
+# Created on: October 2019
+# This module contains constants
+
+HST = 0.13
+Discount = 0.10
+cost_per_item = 100
+minimum_discount = 1000
---
+++ shop.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Created by: Ben Lapuhapo
+# Created on: September 18 2019
+# This program calculates area and circumference.
+#  from user input
+
+import constants
